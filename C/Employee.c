b9d3ac7f7d14ec8c361f3fb05d14dd306732102b
S.Saha <108742981+SoumabhaSaha15@users.noreply.github.com>
2024-01-02 14:20:52 +0530

    Add files via upload

--- C/Employee.c
+++ C/Employee.c
@@ -1,16 +1,8 @@
-#include "./DoubleLinkedList.c"
+#include <stdio.h>
 typedef struct Employee{
   int Id,Salary;
   char Nmae[256];
 }Employee;
-bool IsTrue() {
-  int number;
-  int unused __attribute__((unused));
-  printf("False 0,True 1 [0/1]: ");
-  unused = scanf("%d", &number);
-  fflush(stdin);
-  return ((number == 0) ? false : true);
-}
 Employee *GetEmployee(){
   Employee e;
   printf("enter id: ");
@@ -30,33 +22,4 @@ Employee *GetEmployee(){
 void PrintEmployee(void *Emp){
   Employee *e = (Employee*)Emp;
   printf("{Id : %d,Salary : %d,Name : \"%s\"}\n",e->Id,e->Salary,e->Nmae);
-}
-int main() {
-  List list1;
-  printf("\n!!! ADD 4 MEMBER !!!\n");
-  bool i = false;
-  do {
-    (!i) ? InitiateList(&list1, GetEmployee(), sizeof(Employee))
-         : InsertAtLast(&list1, GetEmployee(), sizeof(Employee));
-    printf("exit False,continue True ");
-    i = IsTrue();
-  } while (i);
-  system("cls");
-  Traverse(&list1, &PrintEmployee);
-  ReverseTraverse(&list1, &PrintEmployee);
-  printf("\n list size : %lu \n", SizeOf(&list1));
-  DeleteAtSpecific(&list1, 0);
-  Traverse(&list1, &PrintEmployee);
-  ReverseTraverse(&list1, &PrintEmployee);
-  printf("\n list size : %lu \n", SizeOf(&list1));
-  DeleteAtSpecific(&list1, 2);
-  Traverse(&list1, &PrintEmployee);
-  ReverseTraverse(&list1, &PrintEmployee);
-  printf("\n list size : %lu \n", SizeOf(&list1));
-  DeleteAtSpecific(&list1, SizeOf(&list1)-1);
-  Traverse(&list1, &PrintEmployee);
-  ReverseTraverse(&list1, &PrintEmployee);
-  DeleteAtSpecific(&list1, SizeOf(&list1));
-  printf("\n list size : %lu \n", SizeOf(&list1));
-  return 0;
 }
