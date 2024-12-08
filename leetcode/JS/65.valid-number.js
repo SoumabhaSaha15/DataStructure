360efc30502fb151f679e89bd29fd3ba3dec3430
S.Saha <soumabhasaha1509+github@gmail.com>
2024-12-09 00:15:55 +0530

    leetcode questions added

---
+++ leetcode/JS/65.valid-number.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=65 lang=javascript
+ *
+ * [65] Valid Number
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const infinityArray = ["-Infinity","Infinity","+Infinity"];
+var isNumber = function(s) {
+  var parsed = parseFloat(s);
+  return !(infinityArray.includes(s) || parsed!=s || Number.isNaN(parsed));
+};
+// @lc code=end
+
