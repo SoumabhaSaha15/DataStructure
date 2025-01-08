b63c1305657e1c63ca390fc14216ef3412743e53
S.Saha <soumabhasaha1509+github@gmail.com>
2025-01-08 21:24:14 +0530

    a small change

--- leetcode/JS/3042.count-prefix-and-suffix-pairs-i.js
+++ leetcode/JS/3042.count-prefix-and-suffix-pairs-i.js
@@ -12,8 +12,8 @@
 const countPrefixSuffixPairs = (words) => {
   let count = 0;  
   for (let i = 0; i < words.length; i++)
-    for (let j = i+0; j < words.length; j++)
-      if (i !== j && words[j].startsWith(words[i]) && words[j].endsWith(words[i])) count++;
+    for (let j = i+1; j < words.length; j++)
+      if (words[j].startsWith(words[i]) && words[j].endsWith(words[i])) count++;
   return count;
 };
 // @lc code=end
